24396834c6a0d7d1bed6ed59d5921e3a20d79e5e
Danilo Del Fio <ddelfio@gmail.com>
2016-12-18 01:12:05 +0100

    configure about sub module

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,17 +2,10 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
-import { AboutUserComponent } from './about/about-user.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes : Routes  = [
-    {  
-        path:'',  
-        redirectTo: '/about',
-        pathMatch: 'full'  },
-    {  path:'about',  component: AboutComponent  },
-    {  path:'about/:username',  component: AboutUserComponent  },
+    {  path:'',  component: HomeComponent },
     {  path:'contact',  component: ContactComponent  },
     {  path:'**',  component: NotFoundComponent  }
 ];
